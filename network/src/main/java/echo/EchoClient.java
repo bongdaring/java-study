9277072434f14fa1a78d107e0f3b6a4ba2009f5d
bongdaring <chlwlsdud258@gmail.com>
2023-08-18 14:48:57 +0900

    echo c/s - mutithread 적용

--- network/src/main/java/echo/EchoClient.java
+++ network/src/main/java/echo/EchoClient.java
@@ -2,9 +2,7 @@ package echo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -13,7 +11,7 @@ import java.net.SocketException;
 import java.util.Scanner;
 
 public class EchoClient {
-	private static final String SERVER_IP = "192.168.0.149"; 
+	private static final String SERVER_IP = "127.0.0.1"; 
 	
 	public static void main(String[] args) {
 		Socket socket = null;
